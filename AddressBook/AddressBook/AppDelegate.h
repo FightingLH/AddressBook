850429cdfa3f305d9c935719a45d4b0e8b7f7656
lihuan <976244150@qq.com>
2017-06-19 11:27:42 +0800

    通讯录

---
+++ AddressBook/AddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by lh on 2017/6/19.
+//  Copyright © 2017年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
